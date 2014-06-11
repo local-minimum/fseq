c3c2b81f258e1de055348b5963a7ac0d55fb6a0e
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-06-11 09:19:26 +0200

    DOC: Started package documentation

---
+++ fseq/__init__.py
@@ -0,0 +1,28 @@
+"""fSeQ is a toolbox for sequence analysis in the frequency domain.
+
+The module is organized around two phases of work: *importing* and *reporting*.
+The importing uses a data format detector `SeqFormatDetector`, a `SeqReader`
+and several `SeqEncoder`s. 
+
+The `Report`s and `ReportBuilder` use the output of the import phase to produce
+the output.
+
+Import
+------
+
+Report
+------
+
+
+See also
+--------
+
+
+References
+----------
+
+Examples
+--------
+
+
+"""
