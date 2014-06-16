56a62b4616bc764bb5d5265b70eda7eff54802b2
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-06-16 14:12:27 +0200

    FEAT: Added a reports base that can be used to create matplotlib figure reports

---
+++ fseq/reporting/reports.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+"""Module for holding the various implemented reporting classes"""
+
+import matplotlib.pyplot as plt
+
+
+class ReportBase(object):
+
+    def __init__(self, name=None, saveArgs=tuple(), saveKwargs=dict()):
+
+        self.name = name
+        self.saveArgs = saveArgs
+        self.saveKwargs = saveKwargs
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def saveArgs(self):
+
+        return self._saveArgs
+
+    @saveArgs.setter
+    def saveArgs(self, args):
+
+        self._saveArgs = args
+
+    @property
+    def saveKwargs(self):
+
+        return self._saveKwargs
+
+    @saveKwargs.setter
+    def saveKwargs(self, kwargs):
+
+        self._saveKwargs = kwargs
+
+    def saveFig(self, fig, outputRoot, outputNamePrefix, name=None,
+            *args, **kwargs):
+        """Saves a figure and creates directories if needed.
+        
+        Parameters
+        ----------
+
+        fig: matplotlib.figure
+            The figure to be saved
+
+        outputRoot: str
+            The root directory for reports
+
+        outputNamePrefix: str
+            If the name of the file should be prepended by some string.
+            Normally added by the *Report Builder*
+
+        name: str, optional
+            A specific name for this figure-file.
+            If none supplied the default name for the report will be used,
+            if no such has been set `ValueError` is raised.
+
+        *args:
+            Any arguments to be sent to `matplotlib.Figure.save`.
+            If none added the `ReportBase.saveArgs` will be used.
+
+        **kwargs:
+            Any keyword arguments to be sent to `matplotlib.Figure.save`.
+            If notn addd the `ReportBase.saveKwargs` will be used.
+
+        Returns
+        -------
+
+        ReportBase
+            Returns `self`
+
+        Raises
+        ------
+
+        ValueError
+            If no name has been given.
+        """
+
+        if name is None:
+            name = self._name
+            if name is None:
+                raise ValueError("Can't save file when no name give to report")
+
+        path = os.path.join(outputRoot, outputNamePrefix + name)
+
+        os.makedirs(path)
+
+        if len(args) == 0:
+            args = self.saveArgs
+
+        if len(kwargs) == 0:
+            kwargs = self.saveKwargs
+
+        fig.savefig(path, *args, **kwargs)
+
+        return self
+
+    def distill(self, data, outputRoot=None, outputNamePrefix=None,
+            *args, **kwargs):
+        """Placeholder distill interface not to be used.
+
+        Raises
+        ------
+
+        NotImplemented
+            Always raises this exception
+        """
+
+        raise NotImplemented("This method should be overwritten")
