359f86f7c809ed41af09e525a5dbe24c247d7dca
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-06-23 15:31:35 +0200

    DOC: Added package doc

--- fseq/__init__.py
+++ fseq/__init__.py
@@ -1,25 +1,78 @@
 """fSeQ is a toolbox for sequence analysis in the frequency domain.
 
-The module is organized around two phases of work: *importing* and *reporting*.
-The importing uses a data format detector `SeqFormatDetector`, a `SeqReader`
-and several `SeqEncoder`s. 
+The module is organized around two phases of work: *reading* and *reporting*.
+
+The reading uses a data format detector `SeqFormatDetector`, which detects
+the current `SeqFormat` that is being read by the `SeqReader` and passed to the
+`SeqEncoder` to translate into a numpy array
 
 The `Report`s and `ReportBuilder` use the output of the import phase to produce
-the output.
+the output. They are organized as such that a builder pre-processes the data
+without any graphics done and the reporters the takes the pre-processed data
+and make displays out of them.
 
-Import
-------
+All relevant parts of *reading* and *reporting* are directly imported to the
+package root.
 
-Report
-------
+Reading
+-------
 
+There is one generic reader that is intended to handle all use cases.
 
-See also
---------
+fseq.SeqReader
+    Root object that coordinates reading, encoding and reporting
 
+The encoders translates and manages format detection
 
-References
+fseq.SeqEncoder
+    Base class encoder
+fseq.SeqEncoderGC
+    Encoder that translates Gs and Cs to 1 while A and T become 0
+
+There's a general format detector, and several data-formats.
+
+fseq.SeqFormatDetector
+    Detector of formats
+fseq.SeqFormat
+    The base class from which all formats must be derived
+fseq.FastaMultiline
+    Format detecting a fasta-file where sequence may span more than one line
+fseq.FastaSingleline
+    Format detecting a fasta-file where sequence is in a signle line
+fseq.FastQ
+    Format detecting fastq, but not which quality encoding
+
+Reporting
+---------
+
+There is a report builder base from which all report builders should be
+made, and two specific report builders.
+
+fseq.ReportBuilderBase
+    The base class for all builders
+fseq.ReportBuilderPositionAverage
+    A report builder that averages data per position
+fseq.ReportBuilderFFT
+    A report builder that subsamples and then does clustered FFT analysis
+
+There are two reports included and an optional base class.
+
+fseq.ReportBase
+    Base class to make constructing new reports more efficient
+fseq.LinePlot
+    Plots a line from the data sent to it
+fseq.HeatMap
+    Plots a heat-map from the data sent to it.
+
+Exceptions
 ----------
+
+fseq.FormatError
+    Base exception for error with data-formats
+fseq.FormatImplementationError
+    If a format is not correctly implemented 
+fseq.FormatUnknown
+    If data is of unknown format
 
 Examples
 --------
